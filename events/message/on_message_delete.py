2eb5908276d73b6128662adc0a888e4faeb57f30
Moaz Haider <90502874+Moazlion@users.noreply.github.com>
2022-04-14 12:00:49 +0000

    Fixed on_message_delete

--- events/message/on_message_delete.py
+++ events/message/on_message_delete.py
@@ -11,14 +11,13 @@ class Delete(commands.Cog):
     async def on_message_delete(self, message):
       if message.author.bot:
         return
-        embeder = nextcord.Embed(title=f"Message deleted in #{message.channel}",description=f"{message.content}",color=teal)
-        embeder.set_author(name=f"{message.author.name}",icon_url=f"{message.author.avatar}")
-        embeder.timestamp = datetime.datetime.utcnow()
-        target = self.client.get_channel(msglogs)
-        if "?purge" in message.content:
-            return
-        else:
-            await target.send(embed=embeder)
+      elif "?purge" in message.content:
+        return
+      embeder = nextcord.Embed(title=f"Message deleted in #{message.channel}",description=f"{message.content}",color=teal)
+      embeder.set_author(name=f"{message.author.name}",icon_url=f"{message.author.avatar}")
+      embeder.timestamp = datetime.datetime.utcnow()
+      target = self.client.get_channel(msglogs)
+      await target.send(embed=embeder)
 
 
 def setup(client):
