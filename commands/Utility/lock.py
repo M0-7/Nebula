fbed59ff8a5a2a59c27e33bc37419e364d41fb06
Moaz Haider <90502874+Moazlion@users.noreply.github.com>
2022-08-17 13:21:52 +0200

    Added button in lock command

--- commands/Utility/lock.py
+++ commands/Utility/lock.py
@@ -2,6 +2,17 @@ import nextcord
 from nextcord.ext import commands
 from config import green
 
+class Unilock(nextcord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    @nextcord.ui.button(label="Unlock", style=nextcord.ButtonStyle.red)
+    async def confirm(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        await interaction.response.send_message("Channel unlocked, Shanny aka 🤡 can see you! Be careful out there.", ephemeral=True)
+        self.value = True
+        self.stop()
+
 class Lock(commands.Cog):
     def __init__(self, client): 
          self.client = client
@@ -10,11 +21,14 @@ class Lock(commands.Cog):
     @commands.has_permissions(administrator=True)
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def lock(self, ctx):
-        embed = nextcord.Embed(title="Channel locked",description="Do *!unlock* to open the channel again",color = green)
-        await ctx.send(embed=embed, delete_after=3)
-        await ctx.message.delete()
         drole = nextcord.utils.get(ctx.guild.roles, name = 'Sasta Sherlock')
         await ctx.channel.set_permissions(drole, view_channel=False)
+        embed = nextcord.Embed(title="Channel locked",description="Do *!unlock* to open the channel again or press the damn button below!",color = green)
+        view=Unilock()
+        msg = await ctx.send(embed=embed,view=view)
+        await view.wait()
+        await ctx.message.delete()
+        await msg.delete()
         
     @lock.error
     async def lock_error(self, ctx, error):
