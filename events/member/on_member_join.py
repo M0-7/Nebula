10830368170bed610e0bdd91ab31f21eae07d89d
Moaz Haider <90502874+Moazlion@users.noreply.github.com>
2022-11-27 10:07:46 +0000

    Update on_member_join.py

--- events/member/on_member_join.py
+++ events/member/on_member_join.py
@@ -1,6 +1,6 @@
 import nextcord
 from nextcord.ext import commands
-from config import blurple,joinLeave,white,chatchannel,announcement,bot_role,member_role
+from config import blurple,joinLeave,white,chatchannel,announcement,bot_role,member_role,join_img
 import datetime
 
 class Join(commands.Cog):
