932a552b48d82ee4b8197106be0c3c2f82f4f5b0
Moaz Haider <90502874+Moazlion@users.noreply.github.com>
2022-12-11 11:45:23 +0000

    Update eight_ball.py

--- Assets/eight_ball.py
+++ Assets/eight_ball.py
@@ -1,3 +1,3 @@
-ball_replies= ["It is certain.","It is decidedly so.","Without a doubt.","Yes - definitely.","You may rely on it.","As I see it, yes.","Most likely.",
-"Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.",
-"Outlook not so good.","Very doubtful."]
+ball_replies= ["yes!!!!","When you grow a braincell, yes","Maybe later", "Trust me kid, you don't want to know","idk","hell nahhh lol","no???","It is known",
+               "Meh","Probably","never, dumbass","It doesn't look good","i dont give two shits","Absolutely.","shit weather ahead 🤡","I am certain of it","That sounds awful",
+               "Definitely","Yes, break out the champagne","Even a blind squirrel finds a nut sometimes","So let it be done"]
