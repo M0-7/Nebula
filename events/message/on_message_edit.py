644a08f831488b041de8d41eb42f50d073863eba
Moaz Haider <90502874+Moazlion@users.noreply.github.com>
2023-01-03 15:29:58 +0000

    Fixed bot sending content without edits

--- events/message/on_message_edit.py
+++ events/message/on_message_edit.py
@@ -12,13 +12,16 @@ class Edit(commands.Cog):
       #Removes bot from logs
       if message_before.author.bot:
         return
-      embedk = nextcord.Embed(title=f"Message edited in #{message_before.channel}",description=f"""
+      elif message_before == message_after:
+        return
+      else:
+        embedk = nextcord.Embed(title=f"Message edited in #{message_before.channel}",description=f"""
     **Before: **{message_before.content}
 **+After: **{message_after.content}""",color=teal)
-      embedk.set_author(name=f'{message_before.author.name}',icon_url=message_before.author.avatar)
-      embedk.timestamp = datetime.datetime.utcnow()
-      target = self.client.get_channel(msglogs)
-      await target.send(embed=embedk)
+        embedk.set_author(name=f'{message_before.author.name}',icon_url=message_before.author.avatar)
+        embedk.timestamp = datetime.datetime.utcnow()
+        target = self.client.get_channel(msglogs)
+        await target.send(embed=embedk)
 
 def setup(client):
     client.add_cog(Edit(client))
